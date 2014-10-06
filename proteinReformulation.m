093a33cb9f18a7c22894e64ee9ba5e51f0010dff
rafaelfang <rafaelfang@live.com>
2014-10-05 20:56:26 -0500

    Protein Reformulation

---
+++ proteinReformulation.m
@@ -0,0 +1,89 @@
+
+% points = 0:pi/50:10*pi;
+% coords(:,3)=points(1:500);
+% coords(:,1) = sin(coords(:,3));
+% coords(:,2) = cos(coords(:,3));
+
+% load Protein;
+% points = Protein;
+% totalPoints=1000;
+% coords=points(1:totalPoints,:);
+load example1
+points=example1;
+totalPoints=500;
+coords=points(1:totalPoints,:);
+
+
+pointsAlign=200;
+
+
+
+half=(totalPoints/2);
+firstHalf=coords(1:half,:);
+secondHalf=coords(half+1:end,:);
+figure;
+subplot(2,2,1);
+plot3(firstHalf(:,1),firstHalf(:,2),firstHalf(:,3));
+hold on
+plot3(secondHalf(:,1),secondHalf(:,2),secondHalf(:,3),'r');
+hold off
+title('Original')
+xlabel('x')
+ylabel('y')
+zlabel('z')
+
+
+
+R=rotx(90);
+secondHalf=R*secondHalf';
+secondHalf=secondHalf';
+subplot(2,2,2);
+plot3(firstHalf(:,1),firstHalf(:,2),firstHalf(:,3));
+hold on
+plot3(secondHalf(:,1),secondHalf(:,2),secondHalf(:,3),'r');
+title('rotated')
+hold off
+xlabel('x')
+ylabel('y')
+zlabel('z')
+
+
+Z=zeros(size(secondHalf));
+Z(:,3)=10;
+secondHalf=secondHalf-Z;
+subplot(2,2,3);
+plot3(firstHalf(:,1),firstHalf(:,2),firstHalf(:,3));
+hold on
+plot3(secondHalf(:,1),secondHalf(:,2),secondHalf(:,3),'r');
+title('shifted')
+hold off
+xlabel('x')
+ylabel('y')
+zlabel('z')
+
+
+[d,Z,transform] = procrustes(firstHalf(half-pointsAlign+1:half,:),secondHalf(1:pointsAlign,:));
+c = transform.c;
+T = transform.T;
+b = transform.b;
+
+secondHalfRecover = b*secondHalf*T+repmat(c(1,:),size(secondHalf,1),1) ;
+    
+
+subplot(2,2,4);
+plot3(firstHalf(:,1),firstHalf(:,2),firstHalf(:,3));
+hold on
+plot3(secondHalfRecover(:,1),secondHalfRecover(:,2),secondHalfRecover(:,3),'r');
+hold off
+title('recoverd from procrustes')
+xlabel('x')
+ylabel('y')
+zlabel('z')
+
+
+
+
+
+
+
+
