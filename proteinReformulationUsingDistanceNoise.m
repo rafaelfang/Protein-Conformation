ab4a7b535246810a4f3d39c453558a73f8e69985
rafaelfang <rafaelfang@live.com>
2014-10-08 22:01:24 -0500

    results

--- proteinReformulationUsingDistanceNoise.m
+++ proteinReformulationUsingDistanceNoise.m
@@ -5,7 +5,7 @@ close all
 % coords(:,3)=points(1:500);
 % coords(:,1) = sin(coords(:,3));
 % coords(:,2) = cos(coords(:,3));
-
+rng(1)
 load Protein;
 points = Protein;
 totalPoints=1000;
@@ -15,12 +15,15 @@ coords=points(1:totalPoints,:);
 % totalPoints=500;
 % coords=points(1:totalPoints,:);
 
-
-pointsAlign=50;
+alpha=1;
+pointsAlign=200;
 
 half=((totalPoints+pointsAlign)/2);
+noise=alpha*randn(half,3);
 firstHalf=coords(1:half,:);
+
 secondHalf=coords(size(firstHalf,1)-pointsAlign+1:end,:);
+
 % figure;
 subplot(2,3,1);
 plot3(firstHalf(:,1),firstHalf(:,2),firstHalf(:,3));
@@ -33,23 +36,38 @@ ylabel('y')
 zlabel('z')
 
 
+
+firstHalf=firstHalf+noise;
+secondHalf=secondHalf+noise;
+subplot(2,3,2);
+plot3(firstHalf(:,1),firstHalf(:,2),firstHalf(:,3));
+hold on
+plot3(secondHalf(:,1),secondHalf(:,2),secondHalf(:,3),'r');
+hold off
+title('add noise to original')
+xlabel('x')
+ylabel('y')
+zlabel('z')
+
+
+
 s1 = pdist2(firstHalf,firstHalf);
 s2 = pdist2(secondHalf,secondHalf);
 s=zeros(totalPoints,totalPoints);
 s(1:half,1:half)=s1;
 s(half-pointsAlign+1:end,half-pointsAlign+1:end)=s2;
-subplot(2,3,2);
+subplot(2,3,3);
 imagesc(s);
 title('s:combine s1 and s2')
 
 s(1:half-pointsAlign,half+1:end)=Inf;
 s(half+1:end,1:half-pointsAlign)=Inf;
-subplot(2,3,3);
+subplot(2,3,4);
 imagesc(s);
 title('s:set two black part into Inf')
 
 s = FastFloyd(s);
-subplot(2,3,4);
+subplot(2,3,5);
 imagesc(s);
 title('s:after applying floyd')
 
@@ -58,7 +76,7 @@ p=cmdscale(s);
 p=p(:,1:3);
 firstHalfRecovered=p(1:half,:);
 secondHalfRecovered=p(size(firstHalfRecovered,1)-pointsAlign+1:end,:);
-subplot(2,3,5);
+subplot(2,3,6);
 plot3(firstHalfRecovered(:,1),firstHalfRecovered(:,2),firstHalfRecovered(:,3));
 hold on
 plot3(secondHalfRecovered(:,1),secondHalfRecovered(:,2),secondHalfRecovered(:,3),'r');
