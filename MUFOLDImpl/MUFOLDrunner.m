6cd87cd56232a353aa536bc58089fba14bbb9270
rafaelfang <cf797@mail.missouri.edu>
2014-12-07 11:10:22 -0600

    mufold runner

--- MUFOLDImpl/MUFOLDrunner.m
+++ MUFOLDImpl/MUFOLDrunner.m
@@ -4,22 +4,22 @@ clear
 %MUFOLD runner
 %written by Chao Fang
 
-load T0659TemplatesData;
+load T0753TemplatesData;
 [ templatesSorted ] = sortTemplateBasedOnEValue( templates,Escores );
 numOfPrimaryTemplates=9;
 primaryTemplateBuilt=cell(numOfPrimaryTemplates,1);
 
 %original plot
-figure;
-for primaryTemplateSelected=1:numOfPrimaryTemplates
-   
-    
-        subplot(3,3,primaryTemplateSelected);
-        temp=templatesSorted{primaryTemplateSelected,1};
-        plot3(temp(:,1),temp(:,2),temp(:,3));
-        title(num2str(primaryTemplateSelected));
-    
-end
+% figure;
+% for primaryTemplateSelected=1:numOfPrimaryTemplates
+%    
+%     
+%         subplot(3,3,primaryTemplateSelected);
+%         temp=templatesSorted{primaryTemplateSelected,1};
+%         plot3(temp(:,1),temp(:,2),temp(:,3));
+%         title(num2str(primaryTemplateSelected));
+%     
+% end
 
 
 
@@ -27,17 +27,23 @@ end
 
 
 figure;
- for primaryTemplateSelected=1:numOfPrimaryTemplates
-    %primaryTemplateSelected=6; %debug use
+for primaryTemplateSelected=1:numOfPrimaryTemplates
+    %primaryTemplateSelected=2; %debug use
     %primaryTemplateSelected=7; %debug use
     %primaryTemplateSelected=8; %debug use
-    [primaryTemplateBuilt{primaryTemplateSelected,1},plotflag]=mufold(templatesSorted,primaryTemplateSelected);
+    [primaryTemplateBuilt{primaryTemplateSelected,1},plotflag,uncoveredHoleInfo]=muFoldSuperPosition(templatesSorted,primaryTemplateSelected);
     if(plotflag==1)
         subplot(3,3,primaryTemplateSelected);
         temp=primaryTemplateBuilt{primaryTemplateSelected,1};
         plot3(temp(:,1),temp(:,2),temp(:,3));
         title(num2str(primaryTemplateSelected));
     else 
-        
+        subplot(3,3,primaryTemplateSelected);
+        temp=primaryTemplateBuilt{primaryTemplateSelected,1};
+        for t=1:size(uncoveredHoleInfo,1)
+            temp(uncoveredHoleInfo(t,1):uncoveredHoleInfo(t,2),:)=NaN;
+        end
+        plot3(temp(:,1),temp(:,2),temp(:,3));
+        title(num2str(primaryTemplateSelected));
     end
- end
+end
